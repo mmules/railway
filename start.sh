61973b8aa62763dd3a3dc145f6fe0dd7fc7a48ae
Jacob Watson <118615181+mmules@users.noreply.github.com>
2022-12-25 08:34:28 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # configs
-AUUID=5aaed9b7-7fe3-47c3-bb52-db59859ce198
+AUUID=40415a59-260f-4768-84d6-639f0429a588
 CADDYIndexPage=https://github.com/PavelDoGreat/WebGL-Fluid-Simulation/archive/master.zip
-CONFIGCADDY=https://raw.githubusercontent.com/test/test/master/etc/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/test/test/master/etc/xray.json
+CONFIGCADDY=https://raw.githubusercontent.com/mmules/railway/master/etc/Caddyfile
+CONFIGXRAY=https://raw.githubusercontent.com/mmules/railway/master/etc/xray.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
 Xray_Newv=`wget --no-check-certificate -qO- https://api.github.com/repos/XTLS/Xray-core/tags | grep 'name' | cut -d\" -f4 | head -1 | cut -b 2-`
 
